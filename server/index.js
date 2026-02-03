b16267949c726da679d3caf009ccbba9fa3cc41e
Acanpal <acanpal52@gmail.com>
2026-02-03 16:43:53 +0900

    feat(server): add express-validator

--- server/index.js
+++ server/index.js
@@ -1,8 +1,9 @@
-require('dotenv').config(); // 環境変数の読み込み
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const todoRoutes = require('./routes/todoRoutes');
-const authRoutes = require('./routes/auth'); // 認証ルートの読み込み
+const authRoutes = require('./routes/authRoutes');
+const authMiddleware = require('./middleware/auth');
 
 const app = express();
 const PORT = 3000;
@@ -10,8 +11,6 @@ const PORT = 3000;
 app.use(cors());
 app.use(express.json());
 
-// ルーティングの設定
-const authMiddleware = require('./middleware/auth'); // ミドルウェアの読み込み
 // /api/auth から始まるURLは authRoutes で処理する
 app.use('/api/auth', authRoutes);
 // /api/todos から始まるURLは authMiddleware でチェックしてから todoRoutes で処理する
